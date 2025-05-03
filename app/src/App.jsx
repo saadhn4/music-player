a754a509cc91d3b390e15ae225213912fbd47dd4
saadhn4 <saadcfi4@gmail.com>
2025-05-03 23:59:40 +0400

    only 1 song

---
+++ app/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useState } from "react";
+import { songs } from "./songs";
+
+const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+  function togglePlay() {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+  }
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+      {songs.map((song, index) => {
+        return (
+          <div
+            className="flex flex-col items-center justify-center bg-white p-4 rounded-xl shadow-2xl w-[300px] max-w-[300px]"
+            key={index}
+          >
+            <img
+              className="w-50 h-50 rounded-lg shadow"
+              src={song.img}
+              alt="song-pic"
+            />
+            <p className="mt-4 font-semibold">{song.title}</p>
+            <audio
+              className="hidden"
+              ref={audioRef}
+              src={song.track}
+              controls
+            ></audio>
+            <div className="flex justify-center items-center pb-2">
+              <button className="font-bold cursor-pointer text-3xl mt-1">
+                ⏮️
+              </button>
+              <button
+                className="font-bold cursor-pointer text-3xl mt-1"
+                onClick={togglePlay}
+              >
+                {isPlaying ? "⏸️" : "▶️"}
+              </button>
+              <button className="font-bold cursor-pointer text-3xl mt-1">
+                ⏭️
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
---
+++ app/src/songs.js
@@ -0,0 +1,10 @@
+import summerPic from "./assets/b0538ae7632778530f7e47672eb74a97.1000x1000x1.png";
+import summer from "./songs/BROCKHAMPTON - SUMMER (STRINGS).mp3";
+export const songs = [
+  {
+    id: 1,
+    title: "Summer - Brockhampton (strings)",
+    img: summerPic,
+    track: summer,
+  },
+];
