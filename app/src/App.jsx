cda4c61795be000e8e150d97479928506fc79966
saadhn4 <saadcfi4@gmail.com>
2025-05-04 00:59:35 +0400

    added 2 songs

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { songs } from "./songs";
 
 const App = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const audioRef = useRef(null);
+  const currentSong = songs[currentIndex];
   function togglePlay() {
     if (isPlaying) {
       audioRef.current.pause();
@@ -13,41 +15,60 @@ const App = () => {
       setIsPlaying(true);
     }
   }
+
+  function toggleNext() {
+    if (currentIndex >= songs.length - 1) return;
+    setCurrentIndex((prev) => prev + 1);
+    setIsPlaying(true);
+  }
+
+  function togglePrev() {
+    if (currentIndex <= 0) return;
+    setCurrentIndex((prev) => prev - 1);
+    setIsPlaying(true);
+  }
+
+  useEffect(() => {
+    if (audioRef.current && isPlaying) {
+      audioRef.current.play();
+    }
+  }, [currentIndex]);
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-gray-100 px-[1rem]">
-      {songs.map((song, index) => {
-        return (
-          <div
-            className="flex flex-col items-center justify-center bg-white p-4 rounded-xl shadow-2xl w-[300px] max-w-[300px] text-center"
-            key={index}
+      <div className="flex flex-col items-center justify-center bg-white p-4 rounded-xl shadow-2xl w-[300px] max-w-[300px] text-center">
+        <img
+          className="w-50 h-50 rounded-lg shadow"
+          src={currentSong.img}
+          alt="song-pic"
+        />
+        <p className="mt-3 font-semibold">{currentSong.title}</p>
+        <audio
+          className="hidden"
+          ref={audioRef}
+          src={currentSong.track}
+          controls
+        ></audio>
+        <div className="flex justify-center items-center pb-2 mt-2">
+          <button
+            onClick={togglePrev}
+            className="font-bold cursor-pointer text-3xl mr-1"
           >
-            <img
-              className="w-50 h-50 rounded-lg shadow"
-              src={song.img}
-              alt="song-pic"
-            />
-            <p className="mt-3 font-semibold">{song.title}</p>
-            <audio
-              className="hidden"
-              ref={audioRef}
-              src={song.track}
-              controls
-            ></audio>
-            <div className="flex justify-center items-center pb-2 mt-2">
-              <button className="font-bold cursor-pointer text-3xl mr-2">
-                ⏮️
-              </button>
-              <button
-                className="font-bold cursor-pointer text-3xl  mr-2"
-                onClick={togglePlay}
-              >
-                {isPlaying ? "⏸️" : "▶️"}
-              </button>
-              <button className="font-bold cursor-pointer text-3xl">⏭️</button>
-            </div>
-          </div>
-        );
-      })}
+            ⏮️
+          </button>
+          <button
+            className="font-bold cursor-pointer text-3xl  mr-1"
+            onClick={togglePlay}
+          >
+            {isPlaying ? "⏸️" : "▶️"}
+          </button>
+          <button
+            onClick={toggleNext}
+            className="font-bold cursor-pointer text-3xl"
+          >
+            ⏭️
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- app/src/songs.js
+++ app/src/songs.js
@@ -1,10 +1,19 @@
 import summerPic from "./assets/b0538ae7632778530f7e47672eb74a97.1000x1000x1.png";
 import summer from "./songs/BROCKHAMPTON - SUMMER (STRINGS).mp3";
+import miaAndSebPic from "./assets/mia and seb pic.png";
+import miaAndSebTheme from "./songs/Mia & Sebastian’s Theme.mp3";
+
 export const songs = [
   {
     id: 1,
     title: "Summer - Brockhampton (strings)",
     img: summerPic,
     track: summer,
+  },
+  {
+    id: 2,
+    title: "Mia & Seb Theme",
+    img: miaAndSebPic,
+    track: miaAndSebTheme,
   },
 ];
